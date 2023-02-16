514c31f1753b534d3a8616585c9db4831d4fd5b4
Uttej2468 <psaiuttej@gmail.com>
2023-02-16 20:44:26 +0530

    added eureka server

---
+++ src/test/java/com/bajaj/eurekaserver/EureksDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajaj.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EureksDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
